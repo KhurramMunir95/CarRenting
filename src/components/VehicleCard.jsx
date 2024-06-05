91427a5e40e4fc844b765070bf8d4be5b90bd5d0
KhurramMunir95 <khurrammunir9522@gmail.com>
2024-06-06 00:41:30 +0500

    Vehicle card section completed

--- src/components/VehicleCard.jsx
+++ src/components/VehicleCard.jsx
@@ -3,14 +3,14 @@ import { Button, Card, Row, Col } from 'react-bootstrap';
 
 const VehicleCard = ({ img }) => {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" className='rounded p-1' src={img} />
+    <Card>
+      <Card.Img variant="top" className='rounded p-2' src={img} />
       <Card.Body>
-        <Card.Title>Lucid Air Pure</Card.Title>
+        <Card.Title className='bold'>Lucid Air Pure</Card.Title>
         <Row className='align-items-center justify-content-between'>
             <Col md={6}>
                 <Card.Text>
-                    <p className='bold'>$100 / Day</p>
+                    <p className='bold m-0'>$100 / Day</p>
                 </Card.Text>
             </Col>
             <Col md={6}>
